c3e1435d2f6e7fe60760fc85f1d0819dfc0dc719
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-08-02 16:12:08 +0100

    add roman numeral  effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
+import React from "react";
 import { useEffect, useState } from "react"
+import { convertToRoman } from "roman";
 
 export default function Home() {
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState <any>("");
+  const [roman, setRoman] = useState <any>("");
 
   const intChange = (e: any) =>{
     setResult(e.target.value)
   }
-  
+  useEffect(() => {
+  setRoman(convertToRoman(result))
+  },)
+
   return (
    <div className="container">
     <h1> CONVERT INTEGER TO ROMAN NUMERALS</h1>
-    <input type="nxumber" placeholder='enter number' onChange={intChange} value={result}/>
+    <input type="number" placeholder='enter number' onChange={intChange} value={result}/>
+    <h3>Roman Numeral:</h3>
+      <h1>{roman}</h1>
    </div>
   )
 }
+
